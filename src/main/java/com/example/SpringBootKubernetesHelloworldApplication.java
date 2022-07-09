9798fe3b6be76087bb86eafff52821508da22c4f
krish <krish@host.docker.internal>
2022-07-09 18:46:18 +0530

    Adding the sample files

---
+++ src/main/java/com/example/SpringBootKubernetesHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKubernetesHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKubernetesHelloworldApplication.class, args);
+	}
+
+}
